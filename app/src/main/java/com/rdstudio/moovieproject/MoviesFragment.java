67a9d9668ffacce7d97162981605c7425653f199
RawzaDigital <rawzadigital@gmail.com>
2019-09-25 13:08:35 +0700

    gambar_not_loaded

--- app/src/main/java/com/rdstudio/moovieproject/MoviesFragment.java
+++ app/src/main/java/com/rdstudio/moovieproject/MoviesFragment.java
@@ -2,6 +2,7 @@ package com.rdstudio.moovieproject;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,12 +16,19 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.rdstudio.moovieproject.adapter.MovieAdapter;
 import com.rdstudio.moovieproject.items.MovieItems;
 import com.rdstudio.moovieproject.model.MovieModel;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.Objects;
+
+import cz.msebera.android.httpclient.Header;
 
 
 /**
@@ -32,6 +40,7 @@ public class MoviesFragment extends Fragment {
     private ProgressBar progressBar;
 
     private MovieModel movieModel;
+    RecyclerView recyclerView;
 
     public MoviesFragment() {
         // Required empty public constructor
@@ -55,31 +64,68 @@ public class MoviesFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_movie, container, false);
 
-        movieModel = ViewModelProviders.of(Objects.requireNonNull(this.getActivity())).get(MovieModel.class);
-        movieModel.getMovies().observe(this, getMovie);
-
         // adapter
-        movieAdapter = new MovieAdapter();
+        movieAdapter = new MovieAdapter(listMovieItems());
         movieAdapter.notifyDataSetChanged();
 
         // Recyclerview
-        RecyclerView recyclerView = rootView.findViewById(R.id.rv_fragment_movie);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        recyclerView = rootView.findViewById(R.id.rv_fragment_movie);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setHasFixedSize(true);
-        recyclerView.setAdapter(movieAdapter);
+        /*recyclerView.setAdapter(movieAdapter);*/
 
         return rootView;
 
     }
 
-    private Observer<ArrayList<MovieItems>> getMovie = new Observer<ArrayList<MovieItems>>() {
+    /*private Observer<ArrayList<MovieItems>> getMovie = new Observer<ArrayList<MovieItems>>() {
         @Override
         public void onChanged(ArrayList<MovieItems> movieItems) {
             if (movieItems != null) {
                 movieAdapter.setData(movieItems);
             }
         }
-    };
+    };*/
 
+    private ArrayList<MovieItems> listMovieItems() {
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        String API_KEY = "5441b84a29b147aebf6253a9a95f82ab";
+
+        final ArrayList<MovieItems> listMovieItems = new ArrayList<>();
+
+        String url = "https://api.themoviedb.org/3/movie/upcoming?api_key=" + API_KEY + "&language=en-US&page=1";
+        client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+
+                    String result = new String(responseBody);
+                    JSONObject responseObject = new JSONObject(result);
+                    JSONArray list = responseObject.getJSONArray("results");
+
+                    for (int i = 0; i < list.length(); i++) {
+                        JSONObject movie = list.getJSONObject(i);
+                        MovieItems movieItems = new MovieItems(movie);
+                        listMovieItems.add(movieItems);
+                        Log.e("onSuccess: ", "Title" + movieItems.getMovieTitle());
+                        if (i==list.length()-1){
+                            recyclerView.setAdapter(movieAdapter);
+                        }
+                    }
+
+
+                } catch (Exception e) {
+                    Log.d("onFailure", Objects.requireNonNull(e.getMessage()));
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Log.d("onFailure", Objects.requireNonNull(error.getMessage()));
+            }
+        });
+        return listMovieItems;
+    }
 
 }
